08f38c81dac4b8ad3b8af9b36b4b4af1e14a3419
ItzelMB <it.mobec@gmail.com>
2019-08-22 18:04:44 -0500

    First commit, adding react app and tailwindcss

---
+++ README.md
@@ -0,0 +1,5 @@
+## Random users
+
+This interface has been build with React JS and Tailwindcss.
+
+You can go to the demo [HERE](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-teal-300">
+      <p>First try with tailwind and react</p>
+    </div>
+  );
+}
+
+export default App;
